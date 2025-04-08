8bee008255a3c64256e78cf92b7bd3b5b383266f
Marcos Schratzenstaller <marksabbath@gmail.com>
2025-04-08 08:36:57 -0300

    Footer content

--- patterns/footer.php
+++ patterns/footer.php
@@ -12,14 +12,110 @@
 <footer class="wp-block-group site-footer">
 	<!-- wp:group {"align":"wide","layout":{"type":"default"}} -->
 	<div class="wp-block-group alignwide">
-		<!-- wp:image {"className":"wp-block-site-logo","lightbox":{"enabled":false},"linkDestination":"none"} -->
-		<figure class="wp-block-image wp-block-site-logo">
-			<img
-				src="<?php echo get_template_directory_uri(); ?>/assets/images/equalstrue-logo.svg"
-				alt="<?php echo esc_attr( get_bloginfo( 'sitetitle' ) ); ?>"
-			/>
-		</figure>
-		<!-- /wp:image -->
+		<!-- wp:group {"align":"full","layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between","verticalAlignment":"top"}} -->
+		<div class="wp-block-group alignfull">
+			<!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}} -->
+			<div class="wp-block-group">
+				<!-- wp:site-title {"level":0} /-->
+
+				<!-- wp:navigation {"overlayMenu":"never","layout":{"type":"flex","orientation":"vertical"}} -->
+					<!-- wp:navigation-link {"label":"Home","url":"#"} /-->
+					<!-- wp:navigation-link {"label":"Opportunities","url":"#"} /-->
+					<!-- wp:navigation-link {"label":"Contact","url":"#"} /-->
+				<!-- /wp:navigation -->
+			</div>
+			<!-- /wp:group -->
+
+			<!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}} -->
+			<div class="wp-block-group">
+				<!-- wp:paragraph {"className":"is-style-heading"} -->
+				<p class="is-style-heading">Discover</p>
+				<!-- /wp:paragraph -->
+
+				<!-- wp:navigation {"overlayMenu":"never","layout":{"type":"flex","orientation":"vertical"}} -->
+					<!-- wp:navigation-link {"label":"Our Story","url":"#"} /-->
+					<!-- wp:navigation-link {"label":"Services","url":"#","kind":"custom"} /-->
+					<!-- wp:navigation-link {"label":"Benefits","url":"#","kind":"custom"} /-->
+					<!-- wp:navigation-link {"label":"Careers","url":"#","kind":"custom"} /-->
+					<!-- wp:navigation-link {"label":"Apply Now","url":"#","kind":"custom"} /-->
+				<!-- /wp:navigation -->
+			</div>
+			<!-- /wp:group -->
+
+			<!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}} -->
+			<div class="wp-block-group">
+				<!-- wp:paragraph {"className":"is-style-heading"} -->
+				<p class="is-style-heading">Legal</p>
+				<!-- /wp:paragraph -->
+
+				<!-- wp:navigation {"overlayMenu":"never","layout":{"type":"flex","orientation":"vertical"}} -->
+					<!-- wp:navigation-link {"label":"Privacy Policy","url":"#"} /-->
+					<!-- wp:navigation-link {"label":"Terms of Use","url":"#","kind":"custom"} /-->
+					<!-- wp:navigation-link {"label":"Candidate Privacy","url":"#","kind":"custom"} /-->
+					<!-- wp:navigation-link {"label":"Sitemap","url":"#","kind":"custom"} /-->
+				<!-- /wp:navigation -->
+			</div>
+			<!-- /wp:group -->
+
+			<!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}} -->
+			<div class="wp-block-group">
+				<!-- wp:paragraph {"className":"is-style-heading"} -->
+				<p class="is-style-heading">Contact</p>
+				<!-- /wp:paragraph -->
+
+				<!-- wp:paragraph -->
+				<p><a href="tel:+14243623033">+1 424 362 3033</a></p>
+				<!-- /wp:paragraph -->
+
+				<!-- wp:paragraph -->
+				<p><a href="mailto:hello@equalstrue.team">hello@equalstrue.team</a></p>
+				<!-- /wp:paragraph -->
+
+				<!-- wp:paragraph -->
+				<p><a href="https://maps.app.goo.gl/QDkveWtfnXfTnGGS6" data-type="link" data-id="https://maps.app.goo.gl/QDkveWtfnXfTnGGS6">Blvd. Miguel de Cervantes<br>Saavedra 169, Granada,<br>Miguel Hidalgo, 11520<br>Ciudad de México, CDMX</a></p>
+				<!-- /wp:paragraph -->
+			</div>
+			<!-- /wp:group -->
+
+			<!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}} -->
+			<div class="wp-block-group">
+				<!-- wp:paragraph {"className":"is-style-heading"} -->
+				<p class="is-style-heading">Social Media</p>
+				<!-- /wp:paragraph -->
+
+				<!-- wp:social-links {"iconBackgroundColor":{},"className":"is-style-default"} -->
+				<ul class="wp-block-social-links is-style-default">
+					<!-- wp:social-link {"url":"https://www.instagram.com/equals_true/","service":"instagram"} /-->
+					<!-- wp:social-link {"url":"https://www.tiktok.com/@equals_true?","service":"tiktok"} /-->
+					<!-- wp:social-link {"url":"https://www.youtube.com/@equalstrue8361","service":"youtube"} /-->
+					<!-- wp:social-link {"url":"https://www.threads.net/@equals_true","service":"threads"} /-->
+					<!-- wp:social-link {"url":"https://www.linkedin.com/company/equalstrue","service":"linkedin"} /-->
+				</ul>
+				<!-- /wp:social-links -->
+
+				<!-- wp:paragraph {"className":"is-style-heading"} -->
+				<p class="is-style-heading">Newsletter</p>
+				<!-- /wp:paragraph -->
+
+				<!-- wp:paragraph -->
+				<p>Be the first to find out about our <br>latest job offers!</p>
+				<!-- /wp:paragraph -->
+			</div>
+			<!-- /wp:group -->
+		</div>
+		<!-- /wp:group -->
+
+		<!-- wp:spacer {"height":"var:preset|spacing|70"} -->
+		<div style="height:var(--wp--preset--spacing--70)" aria-hidden="true" class="wp-block-spacer"></div>
+		<!-- /wp:spacer -->
+
+		<!-- wp:group {"align":"full","style":{"spacing":{"blockGap":"var:preset|spacing|20"}},"layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between"}} -->
+		<div class="wp-block-group alignfull">
+			<!-- wp:paragraph {"fontSize":"small"} -->
+			<p class="has-small-font-size">Copyright © 2023 EQUALS TRUE. All Rights Reserved.</p>
+			<!-- /wp:paragraph -->
+		</div>
+		<!-- /wp:group -->
 	</div>
 	<!-- /wp:group -->
 </footer>
